e182a255662e4f5402175ddf751a564995031211
sahil <sahillakhani266@gmail.com>
2023-12-11 18:46:01 +0530

    fist commit

---
+++ world_clock/lib/pages/loading.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void getData() async {
+    var response = await http.get(Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Kolkata'));
+    Map data = jsonDecode(response.body);
+    print(data);
+
+    String datetime = data['datetime'];
+    String offset = data['offset'];
+  }
+
+  @override
+  void initState(){
+    super.initState();
+    getData();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Text('Loading screen '),
+    );
+  }
+}
