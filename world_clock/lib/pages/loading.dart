2712347681a864495f3fc518d2c9d787c0b24820
sahil <sahillakhani266@gmail.com>
2023-12-13 21:46:08 +0530

    first cvommit to weatther app

--- world_clock/lib/pages/loading.dart
+++ world_clock/lib/pages/loading.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -11,21 +11,26 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
   void getData() async {
-    var response = await http.get(Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Kolkata'));
+    var response = await http
+        .get(Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Kolkata'));
     Map data = jsonDecode(response.body);
     print(data);
 
     String datetime = data['datetime'];
     String offset = data['offset'];
+    print(datetime);
+
+    DateTime now = DateTime.parse(datetime);
+    // print(now);
   }
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     getData();
   }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
